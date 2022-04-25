87dea3133fb11c77a2192be0a945686d48803fa2
Alina <alinaaliaskar51@gmail.com>
2022-04-25 19:03:53 +0600

    added countHandler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
+import { useState } from 'react';
 import './App.css';
 import { Button } from './Component/Button/Button'
 import { Count } from './Component/Count/Count'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <Count counter='0'/>
-      <Button title='+'/>
-      <Button title='-'/>
+      <Count counter={count}/>
+      <Button setCount={setCount} title='+'/>
+      <Button setCount={setCount} title='-'/>
     </div>
   );
 }
--- src/Component/Button/Button.jsx
+++ src/Component/Button/Button.jsx
@@ -2,7 +2,18 @@ import './Button.css'
 
 export const Button = (props) => {
     let title = props.title
+    let setCount =props.setCount
+
+    const countHandler = () => {
+        if (title==='+') {
+            setCount((prev)=>prev+1)
+        }else {
+            setCount((prev)=>prev-1)
+        }
+    }
+
+   
     return(
-        <button className='btn'>{title}</button>
+        <button onClick={countHandler} className='btn'>{title}</button>
     )
 }
